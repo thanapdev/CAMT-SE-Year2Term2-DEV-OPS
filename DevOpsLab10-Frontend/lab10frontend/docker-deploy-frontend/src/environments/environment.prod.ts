451fd4991e5b6f74dc9cf92cbc76cd621b783300
thanapdev <96330641+thanapdev@users.noreply.github.com>
2023-04-03 19:03:36 +0700

    Lab10 Frontend

---
+++ DevOpsLab10-Frontend/lab10frontend/docker-deploy-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  graphqlUri: 'http://54.144.238.181:8085/graphql'
+
+};
---
+++ DevOpsLab10-Frontend/lab10frontend/fontend/docker-deploy-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  graphqlUri: 'http://54.162.51.20:8085/graphql'
+
+};
